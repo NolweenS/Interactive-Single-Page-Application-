4f6b05c0b36a390e335334188379f8aacf3b36c9
NolweenS <nolweensine7@gmail.com>
2025-05-13 16:15:28 +0200

    ik heb mijn main.js verwerkt om de filteroptie op basis van product type toe te voegen nu gaan onze code is het blijft onverandert maar we moeten meer functies toevoegen om de filter optie te kunnen maken en we hebben in onze html ook een dropdown voor een  de product type bijgezet

--- src/main.js
+++ src/main.js
@@ -8,6 +8,13 @@ const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=nyx"
 // Hier gaan we onze "data-list" in onze HTML aanspreken, om daar de producten van de data te plaatsen
 const dataList = document.getElementById("data-list");
 
+//  we maken een filter constante op basis van de type product 
+
+const filterType = document.getElementById("filter-type");
+
+// we willen de data altijd opneiuw roepen das niet effciente dus we bewaren het data hier zodat we bij het filtreren,sorter,.. eenvoudiger data kunnen ophalen
+let allProducts = [];
+
 // we maken een functie om data op te halen voor merk en we doen dit asynchroon zodat JavaScript eerst wacht op de serer en we gebruiken await zodat we op elke stap wachten
 async function fetchData() {
   try {
@@ -20,6 +27,8 @@ async function fetchData() {
 
     //Hier gaan we de data uitprinten om na te kijken of alles klopt (debuggen)
     console.log(data);
+
+    allProducts = data;
 
 
     // we maken een forEach zodat we over de producten kunnen lopen en ze aan de tabel kunnen toevoegen
@@ -53,7 +62,58 @@ async function fetchData() {
 
   }
 
-}
+  async function fetchdata() {
+    try{
+        // const response wacht dat fetchData de data bij de API heeft opgehaald 
+      const response = await fetch(API_URL);
+      
+    // hier gaan we de response van de API_URL omzetten naar Json-data, om het later makkelijker te gebruiken 
+      const data = await response.json();
+      allProducts = data;
+       //Hier gaan we de data uitprinten om na te kijken of alles klopt (debuggen)
+      console.log(data);
+      toonProducten(data);
+    }catch (error){
+      // Bij een fout melding wordt het hier opgenomen en kunnen we het met de console bekijken
+      console.error(" Er ging iets mis bij het ophalen: " + error);
+    }
+    
+  }
+
+  function toonProducten(products){
+    dataList.innerHTML = " ";
+// een forEach loop zodat het over alle producten gaat lopen 
+    products.forEach(product => {
+      // we zetten onze innerhtml die we in de vorige stap hebben gemaakt hier zodat de filteroptie hier op kan werken
+  productCard.innerHTML = `
+
+    <img src = "${product.image_link || "placeholder.jpg"}" alt= "${product.name}" width="100" />
+    <h3>${product.name}</h3>
+    <p> Prijs: €${product.price || "?"}</p>
+    <p>Merk: ${product.brand}</p>
+    <p>Type: ${product.product_type}</p>
+     <p><strong>Beschrijving: </strong> ${product.description || "Geen beschrijving beschikbaar"}</p>
+  `;
+  dataList.appendChild(productCard);
+    });
+    
+
+  }
+
+  // Hier maken we een functie om de producten te kunen filteren 
+  function zetFilters(){
+    const selectedType = filterType.ariaValueMax;
+    let filtered= allProducts;
+    if (selectedType){
+      filtered = filtered.filter(product => product.product_type === selectedType);
+    }
+    toonProducten(filtered);
+  }
+
+  // we zetten een event listener hier zodat de filteroptie kan veranderen
+  filterType.addEventListener("change", zetFilters);
+
+
 
 fetchData();
 
